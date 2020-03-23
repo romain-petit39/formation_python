910c416d291f3549e7de83c15b4bee16a398c9ce
romain-petit39 <45284892+romain-petit39@users.noreply.github.com>
2020-03-23 13:55:25 +0100

    exo3

---
+++ exo3/README.md
@@ -0,0 +1,6 @@
+# formation_python
+
+ python test.py
+ python -m test.py
+ python -c "import test;print(test.name)"
+
---
+++ exo3/convert.py
@@ -0,0 +1,12 @@
+import os
+
+a = '203'
+print("type a", type(a))
+b = 7
+print("type b", type(b))
+a = int(a)
+print("type a", type(a))
+c = a / b
+print("type c", type(c))
+
+os.system("pause")
