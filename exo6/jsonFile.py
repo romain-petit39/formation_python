9acd45ea018ad0dd64f7ce9018b77ac13f067679
romain-petit39 <45284892+romain-petit39@users.noreply.github.com>
2020-03-23 17:15:28 +0100

    correction

--- exo6/jsonFile.py
+++ exo6/jsonFile.py
@@ -3,10 +3,11 @@ import json
 
 f1 = open('file.json'   , 'w', encoding='utf-8', newline="")
 f1.write(json.dumps({"key": "test"}))
+f1.close()
 
 
-file = open("file.json", "r", encoding="utf-8", newline="")
-print(file.read())
+file = open("file.json", "r+", encoding="utf-8", newline="")
+# print(file.read())
 newFile = open("newfile.tx", "w", encoding="utf-8", newline="")
 
 for entry in json.loads(file.read()):
