cb070a8e8c42376a14af9745d30ca57a502318b1
romain-petit39 <45284892+romain-petit39@users.noreply.github.com>
2020-03-24 14:38:57 +0100

    correction

--- exo11/enfant.py
+++ exo11/enfant.py
@@ -1,4 +1,3 @@
-import os
 from parent1 import Parent1
 from parent2 import Parent2
 
@@ -6,4 +5,7 @@ from parent2 import Parent2
 class Enfant(Parent1, Parent2):
     def __init__(self):
         super().__init__()
+
+    def appel(self):
+        self.test()
 
--- exo11/test.py
+++
@@ -1,6 +0,0 @@
-import os
-from enfant import Enfant
-
-enfant = Enfant()
-
-enfant.test()
