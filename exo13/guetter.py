276f9cd7d2e52f9f912cbfa855a7d81c06d533cf
romain-petit39 <45284892+romain-petit39@users.noreply.github.com>
2020-03-25 11:54:13 +0100

    log

--- exo13/guetter.py
+++ exo13/guetter.py
@@ -3,20 +3,23 @@ import sys
 
 
 class Vehicule():
-    logging.basicConfig()
-    log = logging.getLogger("Vehicule")
-    log.setLevel(logging.CRITICAL)
+    
     __roues = 4
+    __log = logging.getLogger("Vehicule")
+
+    def __init__(self):
+        logging.basicConfig()
+        self.__log.setLevel(logging.CRITICAL)
 
     @property
     def nbRoues(self):
-        log.warning('attention')
+        self.__log.warning('attention')
         return self.__roues
 
     @nbRoues.setter
     def nbRoues(self, nbRoue):
         self.__roues = nbRoue
-        log.critical('critique')
+        self.__log.critical('critique')
 
 
     
